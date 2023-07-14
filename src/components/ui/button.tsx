c397edb4bfada1ec2abd51a26d95324f3c8ecf09
Jack <jack.dowww@gmail.com>
2023-07-14 19:17:02 +1000

    Make default button type button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -37,9 +37,9 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>, Var
 type ButtonVariants = VariantProps<typeof buttonVariants>;
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-	({ className, variant, size, asChild = false, ...props }, ref) => {
+	({ className, variant, size, asChild = false, type = "button", ...props }, ref) => {
 		const Comp = asChild ? Slot : "button";
-		return <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />;
+		return <Comp className={cn(buttonVariants({ variant, size, className }))} type={type} ref={ref} {...props} />;
 	},
 );
 Button.displayName = "Button";
