3beaa585a91a3cbd47871c19435a454bfddd3345
Jack <jack.dowww@gmail.com>
2023-07-14 18:14:22 +1000

    Fix auth schema password max length from 10 (testing) too 100 (prod)

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -2,7 +2,7 @@ import * as z from "zod";
 
 const AuthSchema = z.object({
 	email: z.string().email(),
-	password: z.string().min(8).max(10),
+	password: z.string().min(8).max(100),
 });
 type AuthSchema = z.infer<typeof AuthSchema>;
 
